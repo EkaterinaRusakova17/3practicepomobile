1771b8b838c8bde092107d03c0223a859fab8643
EkaterinaRusakova17 <145272537+EkaterinaRusakova17@users.noreply.github.com>
2024-09-26 22:10:27 +0700

    Added MainActivity.kt

--- app/src/main/java/com/example/toasthandler/MainActivity.kt
+++ app/src/main/java/com/example/toasthandler/MainActivity.kt
@@ -1,12 +1,14 @@
-package com.example.toasthandler
+package com.example.linearlayout
 
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.attributes.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +19,39 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val text: EditText = findViewById(R.id.edit_text)
+        val buttonBlack: Button = findViewById(R.id.button_blackText)
+        val buttonRed: Button = findViewById(R.id.button_redText)
+        val button8: Button = findViewById(R.id.button_8sp)
+        val button24: Button = findViewById(R.id.button_24sp)
+        val buttonWhite: Button = findViewById(R.id.button_whiteBackground)
+        val buttonYellow: Button = findViewById(R.id.button_yellowBackground)
+        text.setText("")
+
+        buttonBlack.setOnClickListener {
+            text.setTextColor(Color.BLACK)
+        }
+
+        buttonRed.setOnClickListener {
+            text.setTextColor(Color.RED)
+        }
+
+        button8.setOnClickListener {
+            text.textSize = 8F
+        }
+
+        button24.setOnClickListener {
+            text.textSize = 24F
+        }
+
+        buttonWhite.setOnClickListener {
+            text.setBackgroundColor(Color.WHITE)
+        }
+
+        buttonYellow.setOnClickListener {
+            text.setBackgroundColor(Color.YELLOW)
         }
     }
 }
