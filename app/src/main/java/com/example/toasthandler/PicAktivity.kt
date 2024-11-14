d061bcfa5d0c5b4693f36c37ead597465233cce5
EkaterinaRusakova17 <145272537+EkaterinaRusakova17@users.noreply.github.com>
2024-11-14 21:43:49 +0700

    Create PicAktivity.kt

---
+++ app/src/main/java/com/example/toasthandler/PicAktivity.kt
@@ -0,0 +1,29 @@
+package com.example.linearlayout
+
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+
+class PicActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.pic_layout)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        title = "Картинка"
+
+        val picView: ImageView = findViewById(R.id.picView)
+        val picLink = intent.getStringExtra("picLink")
+
+        Glide.with(this).load(picLink).into(picView)
+    }
+}
