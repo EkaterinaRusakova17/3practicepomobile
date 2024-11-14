a97586bf4f2109ccbf6d52fbd2d9ff98d564a26c
EkaterinaRusakova17 <145272537+EkaterinaRusakova17@users.noreply.github.com>
2024-11-14 21:39:26 +0700

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.toasthandler"
+    namespace = "com.example.linearlayout"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.toasthandler"
+        applicationId = "com.example.linearlayout"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -37,6 +37,8 @@ android {
 
 dependencies {
 
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
